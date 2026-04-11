5937d1025c6d91a30322641761e4c60562e09975
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-11 20:45:03 +0300

    Initilaizing and setting up neccessary packages to learn express advanced security

---
+++ server.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv")
+const { error } = require("winston")
+dotenv.config()
+
+
+app.use(express.json())
+
+mongoose.connect(   process.env.MONGODB_URI)
+.then(() => console.log("Database connected successfully"))
+.catch(error => console.log(error.message))
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`http://localhost:${process.env.port}`)
+})
