fc86f0a5ce30d905d348d5d62abddd7495bf68ed
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-13 12:26:04 +0300

    Implemted ratelimiter and helmet

--- server.js
+++ server.js
@@ -5,20 +5,34 @@ const dotenv = require("dotenv")
 const { error } = require("winston")
 const userRoutes = require("./routes/userRoutes")
 const bookRoutes = require("./routes/bookRoutes")
+const helmet = require("helmet")
 const morgan = require("morgan")
 const winston  = require("winston")
+const rateLimiter = require("express-rate-limit")
 dotenv.config()
 
 
 app.use(express.json())
 
+// database connection
 mongoose.connect(   process.env.MONGODB_URI)
 .then(() => console.log("Database connected successfully"))
 .catch(error => console.log(error.message))
 
+//  app routes handling
 app.use("/api/users", userRoutes)
 app.use("/api/books", bookRoutes)
+
+// security settings
+app.use(helmet())
 app.use(morgan("dev"))
+
+const limiter = rateLimiter({
+    windowMs:15 * 60 * 100,
+    max:100
+})
+
+app.use(limiter)
 
 
 
